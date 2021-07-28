1c23a5a27ae4c3cf9764c54a84343bd1bfa0cfe3
AlexDaughenbaugh <alex_l_daughenbaugh@progressive.com>
2021-07-28 17:04:06 -0400

    done for wednesday

---
+++ BankingTests/OverdraftNotAllowed.cs
@@ -0,0 +1,41 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests
+{
+    public class OverdraftNotAllowed
+    {
+        [Fact]
+        public void OverdraftDoesNotDecreaseBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            try
+            {
+                account.Withdraw(openingBalance + 1);
+            }
+            catch (OverdraftNotAllowedException)
+            {
+
+            }
+            Assert.Equal(openingBalance, account.GetBalance());
+        }
+
+        [Fact]
+        public void OverdraftThrowsException()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            Assert.Throws<OverdraftNotAllowedException>(
+                () => account.Withdraw(openingBalance +1)
+                );
+        }
+    }
+}
